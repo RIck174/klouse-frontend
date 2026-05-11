7aa66c43db056fdc181b6de0a545d7679faf70ab
Richard Buagbe <buagbe48@gmail.com>
2026-05-11 03:54:56 +0000

    fix marker and rides page

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import L from "leaflet";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
-
-L.Icon.Default.mergeOptions({
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
-});
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
