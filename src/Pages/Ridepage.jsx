4e404b84db48f0940a21584ec733d7b1247203a0
Richard Buagbe <buagbe48@gmail.com>
2026-05-12 11:57:07 +0000

    improve

--- src/Pages/Ridepage.jsx
+++ src/Pages/Ridepage.jsx
@@ -161,7 +161,7 @@ function RidePage({ showRideSheet, setShowRideSheet }) {
             )}
 
             <TileLayer
-              url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+              url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
               attribution='&copy; <a href="https://carto.com/">CARTO</a>'
             />
           </MapContainer>
--- src/Pages/Rides.jsx
+++ src/Pages/Rides.jsx
@@ -114,7 +114,7 @@ function Rides() {
           style={{ height: "100vh", width: "100%" }}
         >
           <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
             attribution='&copy; <a href="https://carto.com/">CARTO</a>'
           />
 
